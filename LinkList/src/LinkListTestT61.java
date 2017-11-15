b981cd00ac935d79020bdedd1de3dea199e035be
MafiZo <33687300+MafiZo@users.noreply.github.com>
2017-11-15 15:42:09 +0330

    Create LinkListTestT61.java
    the related test

---
+++ LinkList/src/LinkListTestT61.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LinkListTestT61 {
+@Test
+	public void TestLast611() {
+		LinkList l1=new LinkList();
+		assertEquals(l1.Last,null);
+	}
+}
